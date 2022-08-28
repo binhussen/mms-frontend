2bbad45d97422e4caca19a7f9cb0aeea534db18e
binhussen <mhdhsna@gmail.com>
2022-08-28 20:36:16 +0300

    update ejection response

--- src/app/store/effects/form.effect.ts
+++ src/app/store/effects/form.effect.ts
@@ -86,7 +86,9 @@ export class FormEffect {
             formActions.formSubmittingSuccess({ value: response }),
             tableActions.updateTableColumn({ value: action.value.data }),
           ]),
-          catchError((err) => of(formActions.formSubmittingFailure(err)))
+          catchError((err) =>
+            of(formActions.formSubmittingFailure({ value: err }))
+          )
         )
       )
     )
