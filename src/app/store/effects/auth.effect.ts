b6ee07b9a7769df45c5a4b67b194cfb325a156ab
binhussen <mhdhsna@gmail.com>
2022-09-03 17:58:25 +0300

    Connect auth effect with service

--- src/app/store/effects/auth.effect.ts
+++ src/app/store/effects/auth.effect.ts
@@ -1,16 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, mergeMap, switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
 import authAction from '../actions/auth.action';
+import { AuthState } from '../models/auth.state';
+import { AuthenticationService } from 'src/app/Auths/service/authentication.service';
 
 @Injectable()
 export class AuthEffect {
-  $loginAuth = createEffect(() =>
+  $authSubmit = createEffect(() =>
     this.actions$.pipe(
       ofType(authAction.setLoginForm.type),
-      switchMap((action: { value: any }) => {})
+      switchMap((action: { value: Omit<AuthState, 'response'> }) =>
+        this.authService
+          .login(action.value.data, action.value.submittedToUrl ?? '')
+          .pipe(
+            mergeMap((response) => [
+              authAction.authSuccess({ value: response }),
+              authAction.routeDashboard({ value: response }),
+            ]),
+            catchError((error) => of(authAction.authFailure({ value: error })))
+          )
+      )
     )
   );
 
-  constructor(private actions$: Actions) {}
+  constructor(
+    private actions$: Actions,
+    private authService: AuthenticationService
+  ) {}
 }
