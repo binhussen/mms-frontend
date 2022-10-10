4339c5ce6f5ec42fd8eca3df2c95ab3840ecfcd8
binhussen <mhdhsna@gmail.com>
2022-10-10 11:50:22 +0300

    Role Arrangment

--- src/app/mms-common/molecules/user-menu/user-menu.component.ts
+++ src/app/mms-common/molecules/user-menu/user-menu.component.ts
@@ -1,26 +1,44 @@
-import {Component, ElementRef, HostListener, Input, OnInit} from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Input,
+  OnInit,
+} from '@angular/core';
 import { AuthenticationService } from 'src/app/Auths/service/authentication.service';
 
 @Component({
   selector: 'app-user-menu',
   templateUrl: './user-menu.component.html',
-  styleUrls: ['./user-menu.component.scss']
+  styleUrls: ['./user-menu.component.scss'],
 })
 export class UserMenuComponent implements OnInit {
-
-  @Input() user= {firstName: "Ezedin", lastName: "Fedlu"};
+  @Input() user = { firstName: 'Mohammed', lastName: 'Hussen' };
   @Input() currentUser = null;
   isOpen = false;
   @HostListener('document:click', ['$event', '$event.target'])
   onClick(event: MouseEvent, targetElement: HTMLElement) {
-    if (!targetElement) { return; }
+    if (!targetElement) {
+      return;
+    }
     const clickedInsde = this.elementRef.nativeElement.contains(targetElement);
-    if (!clickedInsde) { this.isOpen = false; }
+    if (!clickedInsde) {
+      this.isOpen = false;
+    }
   }
-  constructor(private elementRef: ElementRef, private authenticationService: AuthenticationService) {
-   //this.authenticationService.logout();
+  role!: string;
+  constructor(
+    private elementRef: ElementRef,
+    private authenticationService: AuthenticationService
+  ) {
+    //this.authenticationService.logout();
   }
   ngOnInit(): void {
+    this.role = this.authenticationService.getRole();
+    console.log('role', this.role);
+    setTimeout(() => {
+      this.role = this.authenticationService.getRole();
+      console.log('role', this.role);
+    }, 2000);
   }
-
 }
